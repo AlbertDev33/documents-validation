c926434fd740bc697ee9ede2b8bced6dbc9ce1d0
Albert <albert.rocha@r2t.company>
2022-03-27 18:46:58 -0300

    improvement: created array validation

--- src/__test__/pis/digitValidation.spec.ts
+++ src/__test__/pis/digitValidation.spec.ts
@@ -13,11 +13,41 @@ const stub = (): ISutType => {
 };
 
 describe('Digit Validation', () => {
+  const pis = [
+    '82965927312',
+    '49053197570',
+    '74231307199',
+    '27184417238',
+    '16458624884',
+    '47587920170',
+    '08288289590',
+    '94247252725',
+  ];
+
+  const invalidPis = [
+    '82965927322',
+    '49053197580',
+    '74231307189',
+    '27184417288',
+    '16458624874',
+    '47587920190',
+    '08288289570',
+    '94247252735',
+  ];
+
   it('should return true is valid pis', () => {
     const { sut } = stub();
-    const pis = '49053197570';
+    pis.forEach(value => {
+      const isValid = sut.execute(value);
+      expect(isValid).toBe(true);
+    });
+  });
 
-    const isValid = sut.execute(pis);
-    expect(isValid).toBe(true);
+  it('should return false is valid pis', () => {
+    const { sut } = stub();
+    invalidPis.forEach(value => {
+      const isValid = sut.execute(value);
+      expect(isValid).toBe(false);
+    });
   });
 });
