e273f1370cb36522168ac89c60aabc558060b8a8
Albert <albert.rocha@r2t.company>
2022-03-27 18:45:52 -0300

    feature: created class instances

---
+++ src/pis/constants/constants.ts
@@ -0,0 +1,19 @@
+import { CleanPis } from '../CleanPis';
+import { DigitValidation } from '../DigitValidation';
+import { IsSequential } from '../IsSequential';
+import { LengthValidation } from '../LengthValidation';
+
+export const EXPECTED_LENGTH = 11;
+export const validateEnum = (digit: number): string | number => {
+  const enumerator = {
+    10: '0',
+    11: '0',
+  };
+
+  return enumerator[digit] || digit;
+};
+
+export const cleanPis = new CleanPis();
+export const isSequential = new IsSequential();
+export const lengthValidation = new LengthValidation();
+export const digitValidation = new DigitValidation();
