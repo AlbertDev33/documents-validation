ebee4128e41c0670ae72631a3c9470a6d590c729
Albert <albert.rocha@r2t.company>
2022-03-27 19:14:14 -0300

    improvement: created consts

--- src/pis/DigitValidation.ts
+++ src/pis/DigitValidation.ts
@@ -1,17 +1,19 @@
+import {
+  validateEnum,
+  EXPECTED_LENGTH,
+  CALC_LENGTH,
+} from './constants/constants';
+
 export class DigitValidation {
   private MULTIPLE_FACTOR = [3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
 
-  private PIS_LENGTH = 11;
-
-  private CALC_LENGTH = 10;
-
   public execute(pis: string): boolean {
     const parsedPis = pis.split('');
-    const digit = parsedPis[this.CALC_LENGTH];
+    const digit = parsedPis[CALC_LENGTH];
 
     const sum = parsedPis.reduce((acc, num, index) => {
       let accumulator = acc;
-      if (index < this.CALC_LENGTH) {
+      if (index < CALC_LENGTH) {
         accumulator += Number(num) * this.MULTIPLE_FACTOR[index];
       }
       return accumulator;
@@ -21,10 +23,9 @@ export class DigitValidation {
   }
 
   private digitVerification(sum: number, digit: string): boolean {
-    const rest = sum % this.PIS_LENGTH;
-    const sub = this.PIS_LENGTH - rest;
-    const verificationDigit =
-      sub === this.CALC_LENGTH || sub === this.PIS_LENGTH ? 0 : sub;
-    return verificationDigit === Number(digit);
+    const rest = sum % EXPECTED_LENGTH;
+    const sub = EXPECTED_LENGTH - rest;
+    const verificationDigit = validateEnum(sub);
+    return Number(verificationDigit) === Number(digit);
   }
 }
